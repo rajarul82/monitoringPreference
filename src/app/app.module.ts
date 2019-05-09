8be0d9ddec9a6ea2d9b4bf0802cd719af0626242
rajarul82 <a.rajarul@outlook.com>
2019-05-09 18:54:14 +0530

    Toastr and Switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule }      from  '@angular/platform-browser/animations';
+import { ToastrModule }     from  'ng6-toastr-notifications';
+import { UiSwitchModule }   from 'ngx-ui-switch';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +21,14 @@ import { PreferenceComponent } from './preference/preference.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    UiSwitchModule.forRoot({
+      size: 'small',
+      checkedLabel: 'on',
+      uncheckedLabel: 'off'
+    }),
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [PreferenceService],
   bootstrap: [AppComponent]
